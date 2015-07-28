5eaec6414ed0daba76931fdbf93a0dc6914f8445
kacso <dsokac1@gmail.com>
2015-07-28 16:31:31 +0200

    uncommited file

---
+++ Boatit/app/src/androidTest/java/co/infinum/academy/danijel_sokac/boatit/tests/Constants/Constants.java
@@ -0,0 +1,9 @@
+package co.infinum.academy.danijel_sokac.boatit.tests.Constants;
+
+/**
+ * Created by Danijel on 28.7.2015..
+ */
+public class Constants {
+    public static final String username = "admin@infinum.co";
+    public static final String password = "infinum1";
+}
